17a2a60b5505637b2edc3561db149d8aab618051
Prashant Agrawal <prashantagrawal2107@gmail.com>
2025-08-08 00:25:43 +0530

    something done --version issues in laptop

--- blog/main.py
+++ blog/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI , Depends , status , Response , HTTPException
 from . import schemas , models
 from .database import engine , SessionLocal
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 app = FastAPI()
 
@@ -23,12 +24,12 @@ def createBlog(request : schemas.Blog , db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog', response_model=list[schemas.ShowBlog])
 def getBlogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}' , status_code=200)
+@app.get('/blog/{id}' , status_code=200 , response_model=schemas.ShowBlog)
 def showBlog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -52,3 +53,15 @@ def updateBlog(id ,request : schemas.Blog , db: Session = Depends(get_db)):
     blog.update({'title':request.title , 'body' : request.body})
     db.commit()
     return 'Done'
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    hashed_password = pwd_context.hash(request.password)
+    request.password = hashed_password
+    new_user = models.User(name=request.name, email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user 
