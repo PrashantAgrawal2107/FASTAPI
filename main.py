8a6ecdec14340aa972db1d5e4458e5e8210c1c96
PrashantAgrawal2107 <prashantagrawal2107@gmail.com>
2025-08-07 18:34:17 +0530

    complete CRUD operations

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool] = None
+
+
+@app.post('/blog')
+def create_blog(request : Blog):
+    return {'data' : 'Blog is created'}
+
+
+@app.get('/blogs')
+def blog(limit:int = 10, pub:bool = True , sort: Optional[str] ='asc'):
+    return {'data' : f'blog list -- {limit} blogs which ar {pub} sorted by {sort}'}
+
+@app.get('/blog/hii')
 def index():
-    return {'data' : {'name' : 'PK'}}
+    return {'data' : 'hii'}
+
+@app.get('/blog/{id}')
+def index(id):
+    return {'data' : {'name' : 'string'}}
+
+# @app.get('/blog/{id}')
+# def index(id:int):
+#     return {'data' : {'name' : 'PK'}}
 
 @app.get('/about')
 def about():
     return 'About Page'
+
+
+
+# ------- RUN USING python3 main.py  -------
+
+# if __name__ == '__main__':
+#     uvicorn.run(app , host = 'localhost' , port=5000)
