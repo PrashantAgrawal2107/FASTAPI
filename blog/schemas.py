dc1cca339430ede97e61c54886398ce7d1a526a4
PrashantAgrawal2107 <prashantagrawal2107@gmail.com>
2025-08-08 14:47:21 +0530

    authentication done

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class Blog(BaseModel):
@@ -34,3 +34,11 @@ class ShowBlog(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
